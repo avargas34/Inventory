feea709a877c4a18fcf4216d289d74a9fd618d1a
Anthony Vargas <Anthony@wlan-t-16-40-dhcp.CSUChico.EDU>
2020-02-12 11:28:44 -0800

    Now includes documentation of the file

--- Inventory.h
+++ Inventory.h
@@ -1,10 +1,32 @@
+/**
+ * @file: Inventory.h
+ *
+ * @brief: Includes the header file for the Inventory class, and maintains
+ * all private and public member functions.
+ *
+ * @Author: Kevin Buffardi
+ * @Author: Anthony Vargas
+ * @date: Feb 12th, 2020
+ */
+
 #ifndef _INVENTORY_
 #define _INVENTORY_
 #include <iostream>
 
 using std::string;
 using std::ostream;
+using std::cout;
+using std::endl;
 
+/**
+ * @brief: The Private member functions are a string for the name
+ * of the object to be sold, a float to hold to price of the item, allowing for
+ * decimals. An int stock to keep track of the amount of Inventory left in
+ * stock.
+ *
+ * @param: none
+ * @returns: none
+ */
 class Inventory
 {
   private:
